1947936af5203ff10a8479f1b031e6b8d6a534f2
zasKoda <zaskoda@gmail.com>
2022-06-07 22:14:06 -0500

    Created Vue3 app with Pinia store, sass compiler, and vite

--- client/README.md
+++ client/README.md
@@ -2,5 +2,38 @@
 
 A Vue.js implementation of a client interface to the Enterprise Cricuit project.
 
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar)
+
+
 ## Getting started
+
+### Setup
+
+- Install node via [nvm](https://github.com/nvm-sh/nvm). (node v18.3+ recommended)
+
+- Install Vite globally:
+
+        npm install -g vite
+
+- Install Hardhat globally:
+
+        npm install -g hardhat
+
+### Common Commands
+
+To serve the client locally
+
+        cd client
+        vite
+
+## Referenes
+
+Vite: https://vitejs.dev/guide/
+
+Vite Templates: https://github.com/vitejs/awesome-vite#templates
+
+SFC: https://v3.vuejs.org/api/sfc-script-setup.html
+
 
---
+++ client/src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import { useTaco } from './stores/taco'
+const taco = useTaco
+</script>
+
+<template>
+  <HelloWorld msg="Orbiter 8" />
+</template>
+
+<style lang="scss">
+body {
+  background: var(--color-bg);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+<style lang="scss">
+//Default Theme
+:root {
+  --color-bg: #ffffff;
+}
+
+//Orbiter 8 Theme
+[data-theme="orbiter8"] {
+  --color-bg: #310e5c;
+}
+
+//Test Theme
+[data-theme="evil"] {
+  --color-bg: #440000;
+}
+</style>
